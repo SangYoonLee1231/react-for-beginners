2eb6a353df0b9c337a33551b35fcbbb3a3de2da3
SangYoonLee <prestro1231@hufs.ac.kr>
2023-07-29 23:15:29 +0900

    Modify: #6.1 실습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("Call The API");
+  }, []);
   return (
     <div>
+      <h1>{counter}</h1>
       <h1 className={styles.title}>Welcome back!!!</h1>
       <Button text={"Continue"} />
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 };
