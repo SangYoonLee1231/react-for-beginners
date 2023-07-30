cb3ad78914686e5c43985d82308bbdbcb11c8afa
SangYoonLee <prestro1231@hufs.ac.kr>
2023-07-30 17:04:48 +0900

    Modify: #6.3 실습 반영

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,19 +4,38 @@ import styles from "./App.module.css";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   console.log("i run all the time");
 
   useEffect(() => {
     console.log("Call The API");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("Search For: ", keyword);
+  }, [keyword]);
+
   return (
-    <div>
-      <h1>{counter}</h1>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
-      <button onClick={onClick}>Click Me</button>
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>{counter}</h1>
+        <h1 className={styles.title}>Welcome back!!!</h1>
+        <Button text={"Continue"} />
+        <button onClick={onClick}>Click Me</button>
+      </div>
+      <div>
+        <input
+          value={keyword}
+          onChange={onChange}
+          type="text"
+          placeholder="Search here..."
+        />
+      </div>
+    </React.StrictMode>
   );
 };
 
